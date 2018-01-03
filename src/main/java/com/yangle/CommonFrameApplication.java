51e8dbf465fc41d7102c49fa2b6dd1becc5d003d
yangle <884454075@qq.com>
2018-01-04 07:11:33 +0800

    初始化

--- src/main/java/com/yangle/CommonFrameApplication.java
+++ src/main/java/com/yangle/CommonFrameApplication.java
@@ -3,17 +3,16 @@ package com.yangle;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.yangle.mapper") //启用mapper扫描
 @SpringBootApplication
-@EnableJms//开启jms注解
+//@EnableJms//开启jms注解
 @EnableCaching//开启缓存机制
 //@EnableScheduling//开启定时器
-public class CommonFrameApplication {
+public class CommonFrameApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommonFrameApplication.class, args);
